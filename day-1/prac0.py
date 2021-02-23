94d5c66a7a543a36d175076171becfbe5f6e6005
sh6863 <kristen6863@gmail.com>
2021-02-23 12:22:42 +0900

    no message

--- day-1/prac0.py
+++ day-1/prac0.py
@@ -50,10 +50,44 @@ print(exit)
 
 
 # 수행시간 측정
+# 선택정렬-기본정렬 수행시간 비교
 import time
+from random import randint
 
+# 배열에 10000개의 정수를 삽입
+array = []
+for _ in range(10000):
+    array.append(randint(1, 100))
+
+# 선택 정렬 프로그램 성능 측정
 start_time = time.time()
-# 실행할 소스코드
-emd_time = time.time()
-print("time: ", emd_time-start_time)
+
+# 선택 정렬 프로그램 소스코드
+for i in range(len(array)):
+    min_index = i           # 기징 작은 원소의 인덱스
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]     # 스와프
+
+emd_time = time.time()      # 측정 종료
+print("선택 정렬 성능 측정: ", emd_time-start_time)        # 수행 시간 출력
+
+
+# 배열을 다시 무작위 데이터로 초기화
+array = []
+for _ in range(10000):
+    array.append(randint(1, 100))
+
+# 기본 정렬 프로그램 성능 측정
+start_time = time.time()
+
+# 기본 정렬 프로그램 소스코드
+array.sort()
+
+emd_time = time.time()  # 측정 종료
+print("기본 정렬 성능 측정: ", emd_time - start_time)  # 수행 시간 출력
+
+
+
 
